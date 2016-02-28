fe8d53259baf093fea1bed8097835817317fcd49
HunterEl <hunterelbourn@gmail.com>
2016-02-27 17:35:00 -0800

    Abstracts the processing of command line arguments out in to a package function.

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("Please provide the necessary flags to use this ding dang tool, ya frank")
-		return
-	}
-	newArgs := os.Args[1:]
-	for i := range newArgs {
-		fmt.Println("Argument ", i, " is: ", newArgs[i])
-		if newArgs[i] == "-h" {
-			fmt.Print("\tYou passed in a header flag\n")
-		} else if newArgs[i] == "-n" {
-			fmt.Print("\tYou passed in a number of requests flag\n")
-		} else if newArgs[i] == "-c" {
-			fmt.Print("\tYou passed in a number of concurrent request flag\n")
-		} else if newArgs[i] == "-m" {
-			fmt.Print("\tYou passed in a request type flag\n")
-		}
-	}
-
+	ProcessArgs(os.Args)
 }
---
+++ processArgs.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ProcessArgs(args []string) {
+	
+	if len(args) == 1 {
+		fmt.Println("Please provide the necessary flags to use this ding dang tool, ya frank")
+		return
+	}
+
+	newArgs := args[1:]
+
+	for i := range newArgs {
+		fmt.Println("Argument ", i, " is: ", newArgs[i])
+		if newArgs[i] == "-h" {
+			fmt.Print("\tYou passed in a header flag\n")
+		} else if newArgs[i] == "-n" {
+			fmt.Print("\tYou passed in a number of requests flag\n")
+
+		} else if newArgs[i] == "-c" {
+			fmt.Print("\tYou passed in a number of concurrent request flag\n")
+		} else if newArgs[i] == "-m" {
+			fmt.Print("\tYou passed in a request type flag\n")
+		}
+	}
+
+}
