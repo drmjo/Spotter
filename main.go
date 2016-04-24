1bad88c4906de908200da29895db9564d77c875b
HunterEl <hunterelbourn@gmail.com>
2016-04-24 16:36:29 -0700

    Adds default request type, average request time and some request type checking.

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"time"
 	"unsafe"
+	"strings"
+	"os"
 )
 
 var headerFlag = flag.String("h", "", "The header for the request")
 var numRequestFlag = flag.Int("n", 1, "The number of requests")
 var concurrentRequestFlag = flag.Int("c", 1, "The number of concurrent requests")
-var requestTypeFlag = flag.String("m", "", "The type of the request")
+var requestTypeFlag = flag.String("m", "GET", "The type of the request")
 
 func main() {
 	flag.Parse()
@@ -20,23 +22,59 @@ func main() {
 	fmt.Println("Number of requests: ", *numRequestFlag, "Address of requests var: ", &numRequestFlag)
 	fmt.Println("Concurrency Level: ", *concurrentRequestFlag, "Address of concurreny var: ", &concurrentRequestFlag)
 	fmt.Println("Request Type: ", *requestTypeFlag, "Adddress of Request type var: ", &requestTypeFlag)
-
-	//Since this returns an array we are going to have to make sure there is something in it before trying to access by index.
 	fmt.Println("Web Server to bench: ", flag.Arg(0))
-	sendRequest(flag.Arg(0), *numRequestFlag)
+	sendRequest(flag.Arg(0), *numRequestFlag, *requestTypeFlag)
 }
 
-func sendRequest(url string, requestNumber int) {
-	defer timer(time.Now(), "Http Request")
+func sendRequest(url string, requestNumber int, requestType string) {
+	client := &http.Client{}
+	timeList := []int64{}
+	determineRequestType(&requestType)
+	req, err := http.NewRequest(requestType, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer timer(time.Now(), "Request time plus setup")
 	for requestNumber > 0 {
-		resp, err := http.Get(url)
+		requestTime := time.Now()
+		resp, err := client.Do(req)
 		if err != nil {
 			log.Fatal(err)
 		}
 		status := resp.Status
-		fmt.Println(status)
+		testTime := time.Since(requestTime)
+		timeList = append(timeList, int64(testTime))
+		fmt.Printf("%s \tRequest time: %s \n", status, testTime)
 		requestNumber--
 	}
+	fmt.Printf("Average request time was: %s \n", determineAverageRequestTime(timeList))
+}
+
+func determineAverageRequestTime(timeList []int64) time.Duration {
+	var averageTime int64
+	for i := range timeList {
+		averageTime += timeList[i]
+	}
+	return time.Duration(averageTime/int64(len(timeList)))
+}
+
+func determineRequestType(requestType *string) {
+	switch strings.ToUpper(*requestType) {
+	case "GET":
+		*requestType = "GET"
+		return
+	case "POST":
+		*requestType = "POST"
+		return
+	case "PUT":
+		*requestType = "PUT"
+		return
+	case "DELETE":
+		*requestType = "DELETE"
+		return
+	}
+	log.Fatalf("Cannot process the request type of %s", *requestType)
+	os.Exit(1)
 }
 
 func timer(start time.Time, name string) {
