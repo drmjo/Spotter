fa465fad4e4b5b2bba3b0d3e85727683009d9b51
HunterEl <hunterelbourn@gmail.com>
2016-04-24 14:28:05 -0700

    Getting the status code back from requests.

--- main.go
+++ main.go
@@ -3,34 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"unsafe"
 )
 
-// Entry point to our dope program.
+var headerFlag = flag.String("h", "", "The header for the request")
+var numRequestFlag = flag.Int("n", 1, "The number of requests")
+var concurrentRequestFlag = flag.Int("c", 1, "The number of concurrent requests")
+var requestTypeFlag = flag.String("m", "", "The type of the request")
+
 func main() {
-
-
-	// TODO: Think about what the default header for this should be
-	headerFlag := flag.String("h", "", "The header for the request")
-
-	// Just hit the endpoint once
-	numRequestFlag := flag.Int("n", 1, "The number of requests")
-
-	// Probably concurrency level 1 unless otherwise specified
-	concurrentRequestFlag := flag.Int("c", 1, "The number of concurrent requests")
-
-	// TODO: Think about default request type. GET?
-	requestTypeFlag := flag.String("m", "", "The type of the request")
-
-	// Parse the command line flags
 	flag.Parse()
+	fmt.Println("Header: ", *headerFlag, "Address of Header Var: ", &headerFlag, unsafe.Sizeof(headerFlag))
+	fmt.Println("Number of requests: ", *numRequestFlag, "Address of requests var: ", &numRequestFlag)
+	fmt.Println("Concurrency Level: ", *concurrentRequestFlag, "Address of concurreny var: ", &concurrentRequestFlag)
+	fmt.Println("Request Type: ", *requestTypeFlag, "Adddress of Request type var: ", &requestTypeFlag)
 
-	// Make sure we are getting what we 
-	fmt.Println("Header: ", *headerFlag)
-	fmt.Println("Number of requests: ", *numRequestFlag)
-	fmt.Println("Concurrency Level: ", *concurrentRequestFlag)
-	fmt.Println("Request Type: ", *requestTypeFlag)
-	
-	// Since this returns an array we are going to have to make sure there is something in it before trying to access by index.
-	fmt.Println("Web Server to bench: ", flag.Args())
+	//Since this returns an array we are going to have to make sure there is something in it before trying to access by index.
+	fmt.Println("Web Server to bench: ", flag.Arg(0))
+	createClient(flag.Arg(0))
 }
 
+func createClient(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	status := resp.Status
+	fmt.Printf("%s", status)
+}
